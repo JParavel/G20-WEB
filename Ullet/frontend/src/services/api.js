22d08a19e05cc5cb503d3545e1f7fd9c4b02f309
JParavel <jgamezc@uninorte.edu.co>
2022-12-22 21:13:15 -0500

    Se agregó una caracteristica especial a los servicios

--- Ullet/frontend/src/services/api.js
+++ Ullet/frontend/src/services/api.js
@@ -1,1 +1,21 @@
 export const API_URL = import.meta.env.VITE_API_URL;
+
+class api {
+  async get(path, options) {
+    try {
+      const res = await fetch(API_URL + path, options);
+      const document = await res.json();
+      if (res.ok) {
+        return document;
+      } else {
+        console.log(document.error);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  post() {}
+  patch() {}
+  delete() {}
+}
